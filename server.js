1d512b2b5dd0e38721fc88788ff73e6f0855a161
ilinoa <ilinoa90@gmail.com>
2020-06-06 01:56:43 +0300

    deploy ok

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-
-const { ExpressPeerServer } = require('peer');
-
-
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5001;
 
 const express = require('express');
 const path = require('path');
@@ -11,14 +7,5 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.listen(PORT);
 
-const server = app.listen(PORT);
-
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: '/myapp'
-});
-
-
-
-app.use('/peerjs', peerServer);
