711e508824ca2fff54a4863bf7837793a42f23f4
ilinoa <ilinoa90@gmail.com>
2020-06-05 21:25:35 +0300

    npm start

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 
-app.listen(process.env.PORT || PORT, ()=>console.log(`App has been started on port ${5000}...`))
+return app.listen( PORT, ()=>console.log(`App has been started on port ${5000}...`));
+    
