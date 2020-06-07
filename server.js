7ea28cec47152e1b4a327c69ecf65d2a049e9e55
ilinoa <ilinoa90@gmail.com>
2020-06-07 19:11:15 +0300

    добавил socket.io

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const PORT = process.env.PORT || 5000;
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(PORT);
+
+
+/*
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -14,3 +27,5 @@ app.get('*', function(req, res) {
 });
 server.listen(PORT);
 // "proxy":"http://localhost:5000",
+
+*/
