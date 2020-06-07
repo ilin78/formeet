603311a4a927e109edcd42dc4ba6b8231ce5dc7e
ilinoa <ilinoa90@gmail.com>
2020-06-07 21:49:30 +0300

    изменнил socket server

--- server.js
+++ server.js
@@ -11,15 +11,19 @@ app.get('*', function(req, res) {
 app.listen(PORT);
 */
 
+
+
+
+
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT);
-const io = require ('socket.io').listen(server);
 
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
 io.on('connection', socket => console.log(socket.id) );
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -27,5 +31,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 console.log('port:5000 Server started...')
+server.listen(PORT);
 
-// app.listen(PORT);
+
+
+
+
+
+io.on('connection', () => { /* … */ });
