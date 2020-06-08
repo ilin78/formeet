02493b10b11c5a716d9e167b3d2477cd63a0eac2
ilinoa <ilinoa90@gmail.com>
2020-06-08 23:53:36 +0300

    адаптировал socket.io по heroku

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const path = require('path');
 const PORT = process.env.PORT || 5000;
+const INDEX = '/index.html';
+const express = require('express');
+const path = require('path');
+const arrPeerId = [];
+//___________________server______________________________________________
+
+const server = express()
+    .use(express.static(path.join(__dirname, 'build')))
+    .get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//__________________io___________________________________________________
+
 const io = require ('socket.io').listen(server);
 
-const arrPeerId = [];
-
 io.on('connection', socket => {
-    socket.emit('ONLINE_PEER_ARRAY', arrPeerId)
+  socket.emit('ONLINE_PEER_ARRAY', arrPeerId)
 
-    socket.on('NEW_PEER_ID', peerId => {
-      socket.peerId = peerId
-      arrPeerId.push(peerId);
-      
-      io.emit('NEW_CLIENT_CONNECT', peerId, console.log("NEW_CLIENT_CONNECT:",peerId));
+  socket.on('NEW_PEER_ID', peerId => {
+    socket.peerId = peerId
+    arrPeerId.push(peerId);
+    
+    io.emit('NEW_CLIENT_CONNECT', peerId, console.log("NEW_CLIENT_CONNECT:",peerId));
 
-    });
+  });
 
-    socket.on('disconnect', () => {
-      const index = arrPeerId.indexOf(socket.peerId);
-      arrPeerId.splice(index, 1);
-      io.emit ('SOMEONE_DISCONNECTED', socket.peerId)
-    });
+  socket.on('disconnect', () => {
+    const index = arrPeerId.indexOf(socket.peerId);
+    arrPeerId.splice(index, 1);
+    io.emit ('SOMEONE_DISCONNECTED', socket.peerId)
+  });
 
 });
+//_________________end__________________________________________________
 
-app.use(express.json({ extendet: true}));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-}
-server.listen(PORT);
-console.log('http://localhost:'+`${PORT}`)
