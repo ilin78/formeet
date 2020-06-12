d1035e65129b049ee47cd7e9c6eeb61e3b762e64
ilinoa <ilinoa90@gmail.com>
2020-06-12 19:36:23 +0300

    добавил пакет config

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = config.get('port') || process.env.PORT || 5000
 
 const arrPeerId = [];
 
+
+
 const NEW_PEER_ID = 'NEW_PEER_ID';
 const ONLINE_PEER_ARRAY = 'ONLINE_PEER_ARRAY';
 const NEW_CLIENT_CONNECT = 'NEW_CLIENT_CONNECT';
